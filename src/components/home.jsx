0c6a0ab9f77b41d7da3024b9597fed5b73efa89f
akashcn04 <akashnyamagoud45@gmail.com>
2024-10-03 23:12:40 +0530

    updated home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -15,9 +15,10 @@ import { ReactTyped } from "react-typed";
 
 function home() {
   return (
-    <div className='max-w-[1320px] h-14 mx-auto my-[50px] md:my-[150px]'>
+    <>
+    <div className='max-w-[1320px] mx-auto my-[50px] md:mt-[150px]'>
       <div className='flex flex-col md:flex-row justify-between'>
-        <div className='px-10 md:px-0 py-0 md:py-4 order-2 md:order-1 basis-[40%]'>
+        <div className='px-10 md:px-0 py-0 md:py-2 order-2 md:order-1 basis-[40%]'>
           <p className='text-xl pt-4 md:pt-12'>
             Welcome In My Feed. 
 
@@ -34,12 +35,12 @@ function home() {
           <p className='py-3 text-sm md:text-md text-justify'>
             olhwofnhfifj aiouefhbiuifjwebfij bsndfiubaifgnjrbguiir aufgiajbfijb bfibdcnjisbdfb ugfuigbu sbfbbfuiiwebfiubei wuegfoiqjhwirnq jdfdfyqweg qiuwgdf8qgwdf njhqwvdfgq qwyfvgqbw ijdby8qwdknqog ajksfgvi bwsajnsoiahdnd aousdbuisbdjansiduioasud qabwdiuassdnuiajbndbn obwdfa sod
           </p>
-          <div className='flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 mt-2 pl-[20%] md:pl-0'>
+          <div className='flex flex-col items-center md:flex-row justify-between space-y-5 md:space-y-0 mt-2'>
             <div>
-              <p>
+              <p className='ml-3 md:ml-0 text-center md:text-left'>
                 Available on
               </p>
-              <div className='flex'>
+              <div className='flex space-x-2'>
                 <a href="https://www.linkedin.com/in/akash-nyamagoud-2073b9260/">
                   <FaLinkedin className='text-xl md:text:2xl mt-2 hover:scale-[150%] duration-[0.5s]' />
                 </a>
@@ -59,11 +60,11 @@ function home() {
               </div>
             </div>
             <div >
-              <p>
+              <p text-center>
                 Currently Working On
               </p>
-              <div className='flex pl-0'>
-                <a href="https://react.dev/" class="flex items-center">
+              <div className='flex ml-1 md:ml-0 space-x-2'>
+                <a href="https://react.dev/">
                   <SiReact className='text-xl md:text:2xl mt-2 hover:scale-[150%] duration-[0.5s]' />
                 </a>
                 <a href="https://nodejs.org/en">
@@ -85,12 +86,13 @@ function home() {
           </div>
         </div>
         <div className='order-1 md:order-2 basis-[60%]'>
-          <img src="src/assets/coder.png" alt="" className=' w-[450px] h-[450px] mx-auto mr-[100px] ' />
+          <img src="src/assets/coder.png" alt="" className=' w-[450px] h-[450px] mx-auto mr-[100px] rounded-full bg-slate-300' />
         </div>
-
       </div>
     </div>
-  )
+    <hr className="border-black border-1" />
+    </>
+  );
 }
 
 export default home
